d26b62027fbcf6428815225551cb9544a2aa47a6
Trevor <trevorgillette17@gmail.com>
2018-10-23 17:55:20 -0700

    "test"

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,75 +1,53 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class MessageList extends Component {
   constructor (props) {
     super(props);
 
     this.state = {
-      messages: [],
-      newMessagesName: ''
+      messages: []
     };
 
-    this.messageRef = this.props.firebase.database().ref('messages');
+    this.messagesRef = this.props.firebase.database().ref('messages');
   }
 
-  //Fetch rooms from the database
+  //Fetch messages from the database
   componentDidMount() {
-    this.messageRef.on('child_added', snapshot => {
+    this.messagesRef.on('child_added', snapshot => {
       const message = snapshot.val();
       message.key = snapshot.key;
       this.setState({ messages: this.state.messages.concat( message ) });
+      console.log(this.state.messages);
     });
   }
 
-  //Controlled Component, keep the React state as the 'single source of truth'
-  handleChange(event) {
-    this.setState({newMessageName: event.target.value});
-  }
-
-  //Create the new room in Firebase
-  //Prevent refresh
-  //Reset newRoomName
-  createMessage(event) {
-    event.preventDefault();
-    if (!this.state.newMessageName) { return }
-    this.messageRef.push({ name: this.state.newMessageName });
-    this.setState({ newMessageName: '' });
-  }
-
-  handleClick(event) {
-    let messageName = event.target.innerText;
-    let messageId = '';
-    this.messageRef.orderByChild("name").equalTo(messageName).on('child_added', snapshot => {
-      messageId = snapshot.key;
-    });
-    this.props.activeMessageView(messageId, messageName);
-  }
-//Create a row for each room in the database
-//User input to create a new room in the database and render it
-//Handle clicks on room names
+  //Create message table with header
+  //Filter messages using database UID then map them to table rows
   render() {
-    return(
+    return (
       <table id="message-list">
         <colgroup>
-          <col id="message-name"/>
+          <col id="username-message" />
+          <col id="timestamp" />
         </colgroup>
-        <tbody>
+        <thead>
+          <tr>
+            <th>{this.props.activeRoomName}</th>
+          </tr>
+        </thead>
           {
-            this.state.messages.map( (message, index) =>
-              <tr className="message" key={index} onClick= { (e) => this.handleClick(e) }>
-                <td>{message.name}</td>
+            this.state.messages.filter( message => message.roomId == this.props.activeRoomId ).map( (message, index) =>
+            <tbody>
+              <tr key={index}>
+                <td className="username">{message.username}</td>
+                <td className="timestamp">{message.sentAt}</td>
               </tr>
+              <tr className="message" key={index}>
+                <td>{message.content}</td>
+              </tr>
+            </tbody>
             )
           }
-          <tr>
-            <td>
-              <form onSubmit={ (e) => this.createMessage(e)}>
-                <input type="text" name="name" placeholder="New message" value={this.state.newMessageName} onChange={ (e) => this.handleChange(e) } />
-                <input type="submit" value="Create" />
-              </form>
-            </td>
-          </tr>
-        </tbody>
       </table>
     );
   }
